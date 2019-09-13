d23363d982f7b09a084614150d0484e685d55f14
mtom92 <torresmoranm@gmail.com>
2019-09-12 22:53:09 -0700

    changed with es6

--- trie.js
+++ trie.js
@@ -1,141 +1,89 @@
-// Trie.js - super simple JS implementation
-// https://en.wikipedia.org/wiki/Trie
-
-// -----------------------------------------
-
-// we start with the TrieNode
-function TrieNode(key) {
-  // the "key" value will be the character in sequence
-  this.key = key;
-
-  // we keep a reference to parent
-  this.parent = null;
-
-  // we have hash of children
-  this.children = {};
-
-  // check to see if the node is at the end
-  this.end = false;
+class TrieNode{
+    constructor(){
+        this.isEnd = false
+        this.freq = 0
+        this.children = {}
+    }
 }
 
-// iterates through the parents to get the word.
-// time complexity: O(k), k = word length
-TrieNode.prototype.getWord = function() {
-  var output = [];
-  var node = this;
+class Trie{
+    constructor(){
+        this.root = new TrieNode()
+    }
 
-  while (node !== null) {
-    output.unshift(node.key);
-    node = node.parent;
-  }
+    insert(word){
+        if( word.length === 0 ) return 	// forbid empty string
+        let letter;
+        let curNode = this.root
 
-  return output.join('');
-};
+        for( let i = 0; i < word.length; i++ ){
+            letter = word[i]
+            if( !curNode.children.hasOwnProperty(letter) ){
+                curNode.children[letter] = new TrieNode()
+            }
+            curNode = curNode.children[letter]
+        }
+        curNode.isEnd = true
+        curNode.freq = curNode.freq + 1
+    }
 
-// -----------------------------------------
+    getNodeForPrefix(str){
+        let letter, curNode = this.root
+        for( let i = 0; i < str.length; i++ ){
+            letter = str[i]
+            if( !curNode.children.hasOwnProperty(letter) )
+                return null
+            curNode = curNode.children[letter]
+        }
+        return curNode
+    }
 
-// we implement Trie with just a simple root with null value.
-function Trie() {
-  this.root = new TrieNode(null);
+    isWord(word){
+        if( this.getNodeForPrefix(word) === null ) return false
+        return this.getNodeForPrefix(word).isEnd
+    }
+
+    getWordFreq(word){
+        if( this.getNodeForPrefix(word) === null ) return false
+        return this.getNodeForPrefix(word).freq
+    }
 }
 
-// inserts a word into the trie.
-// time complexity: O(k), k = word length
-Trie.prototype.insert = function(word) {
-  var node = this.root; // we start at the root 😬
 
-  // for every character in the word
-  for(var i = 0; i < word.length; i++) {
-    // check to see if character node exists in children.
-    if (!node.children[word[i]]) {
-      // if it doesn't exist, we then create it.
-      node.children[word[i]] = new TrieNode(word[i]);
+/* -------------------- Testing --------------------*/
 
-      // we also assign the parent to the child node.
-      node.children[word[i]].parent = node;
-    }
+let ace = "ace"
+let at = "at"
+let cat = "cat"
+let cab = "cab"
+let of = "of"
 
-    // proceed to the next depth in the trie.
-    node = node.children[word[i]];
+let t = new Trie()
+t.insert(ace)
+console.log(t.isWord(ace))	  // true
 
-    // finally, we check to see if it's the last word.
-    if (i == word.length-1) {
-      // if it is, we set the end flag to true.
-      node.end = true;
-    }
-  }
-};
+t.insert(ace)
+console.log(t.isWord(ace))     // true
+console.log(t.getWordFreq(ace))// 2
 
-// check if it contains a whole word.
-// time complexity: O(k), k = word length
-Trie.prototype.contains = function(word) {
-  var node = this.root;
+t.insert(at)
+console.log(t.isWord(at))      // true
+console.log(t.getWordFreq(at)) // 1
 
-  // for every character in the word
-  for(var i = 0; i < word.length; i++) {
-    // check to see if character node exists in children.
-    if (node.children[word[i]]) {
-      // if it exists, proceed to the next depth of the trie.
-      node = node.children[word[i]];
-    } else {
-      // doesn't exist, return false since it's not a valid word.
-      return false;
-    }
-  }
+t.insert(cat)
+t.insert(cat)
+console.log(t.isWord(cat))     // true
+console.log(t.getWordFreq(cat))// 2
+t.insert(cat)
+console.log(t.getWordFreq(cat))// 3
 
-  // we finished going through all the words, but is it a whole word?
-  return node.end;
-};
+t.insert(cab)
+console.log(t.isWord(cab))     // true
+console.log(t.getWordFreq(cab))// 1
 
-// returns every word with given prefix
-// time complexity: O(p + n), p = prefix length, n = number of child paths
-Trie.prototype.find = function(prefix) {
-  var node = this.root;
-  var output = [];
+t.insert(of)
+console.log(t.isWord(of))     // true
+console.log(t.getWordFreq(of))// 1
 
-  // for every character in the prefix
-  for(var i = 0; i < prefix.length; i++) {
-    // make sure prefix actually has words
-    if (node.children[prefix[i]]) {
-      node = node.children[prefix[i]];
-    } else {
-      // there's none. just return it.
-      return output;
-    }
-  }
-
-  // recursively find all words in the node
-  findAllWords(node, output);
-
-  return output;
-};
-
-// recursive function to find all words in the given node.
-function findAllWords(node, arr) {
-  // base case, if node is at a word, push to output
-  if (node.end) {
-    arr.unshift(node.getWord());
-  }
-
-  // iterate through each children, call recursive findAllWords
-  for (var child in node.children) {
-    findAllWords(node.children[child], arr);
-  }
-}
-
-// -----------------------------------------
-
-// instantiate our trie
-var trie = new Trie();
-
-// insert few values
-trie.insert("hello");
-trie.insert("helium");
-
-// check contains method
-console.log(trie.contains("helium"));  // true
-console.log(trie.contains("kickass")); // false
-
-// check find method
-console.log(trie.find("hel"));  // [ 'helium', 'hello' ]
-console.log(trie.find("hell")); // [ 'hello' ]
+// check the internal representation of the Trie
+console.log(t.root)
